69ea339b3b2ea4a12b930214ca78e641e15b9dc1
sirlocust <jua56yu@hotmail.com>
2021-10-12 20:39:27 -0500

    configuracion inicial del frontend

--- frontEnd/src/App.tsx
+++ frontEnd/src/App.tsx
@@ -1,10 +1,7 @@
-import { Counter } from './Counter'
-
 export const App = () => {
   return (
     <div>
       <h1>React TypeScript Webpack Starter Template</h1>
-      <Counter />
     </div>
   )
 }
